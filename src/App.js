769c6a0782489eae74f99a8b21dac203bf0a1686
dumindapriyasad <duminda.priyasad@gmail.com>
2022-06-24 21:44:52 +0530

    Initialize App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { VStack, IconButton, Box, Heading, useColorMode } from '@chakra-ui/react';
+import { FaSun, FaMoon } from "react-icons/fa";
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState(() => JSON.parse(localStorage.getItem('todos')) || []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  function deleteTodo(id) {
+    const newTodos = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+
+    setTodos(newTodos);
+  }
+
+  function addTodo(todo) {
+    setTodos([...todos, todo]);
+  }
+
+  const {colorMode, toggleColorMode} = useColorMode();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack p='4'>
+      <IconButton icon={colorMode === 'light' ? <FaSun /> : <FaMoon />} isRound='true' size='lg' alignSelf='flex-end' onClick={toggleColorMode} />
+      <Box>
+        <Heading mb='8' fontWeight='extrabold' size='2xl' bgGradient='linear(to-r, cyan.400, purple.400, pink.400)' bgClip='text'>Todo Application</Heading>
+      </Box>
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+      <AddTodo addTodo={addTodo} />
+    </VStack>
   );
 }
 
